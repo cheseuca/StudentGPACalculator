3cbedf4a1b8842753b7ee123ab38204cb4864e60
Chesca <150506251+cheseuca@users.noreply.github.com>
2024-04-09 23:38:08 +0800

    Modified the GUI and added new methods
    The application was mainly used to calculate the final GPA with a fixed credit hours if we're basing it on the C# Microsoft Learn Curriculum (Get Started with C#, Part 1).  Added new grade for more options.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,75 +29,35 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox6 = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StudentName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.finalGPA = new System.Windows.Forms.Label();
+            this.EnglishGrade = new System.Windows.Forms.TextBox();
+            this.BiologyGrade = new System.Windows.Forms.TextBox();
+            this.AlgebraGrade = new System.Windows.Forms.TextBox();
+            this.ComputerScienceGrade = new System.Windows.Forms.TextBox();
+            this.PsychologyGrade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 361);
+            this.button1.Location = new System.Drawing.Point(28, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(202, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calculate GPA";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(188, 110);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(122, 22);
-            this.maskedTextBox1.TabIndex = 1;
-            this.maskedTextBox1.Text = "Enter your Grade";
-            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
-            // 
-            // maskedTextBox3
-            // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(188, 282);
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(122, 22);
-            this.maskedTextBox3.TabIndex = 3;
-            this.maskedTextBox3.Text = "Enter your Grade";
-            this.maskedTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // maskedTextBox4
-            // 
-            this.maskedTextBox4.Location = new System.Drawing.Point(188, 240);
-            this.maskedTextBox4.Name = "maskedTextBox4";
-            this.maskedTextBox4.Size = new System.Drawing.Size(122, 22);
-            this.maskedTextBox4.TabIndex = 4;
-            this.maskedTextBox4.Text = "Enter your Grade";
-            this.maskedTextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // maskedTextBox5
-            // 
-            this.maskedTextBox5.Location = new System.Drawing.Point(188, 197);
-            this.maskedTextBox5.Name = "maskedTextBox5";
-            this.maskedTextBox5.Size = new System.Drawing.Size(122, 22);
-            this.maskedTextBox5.TabIndex = 5;
-            this.maskedTextBox5.Text = "Enter your Grade";
-            this.maskedTextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // maskedTextBox6
-            // 
-            this.maskedTextBox6.Location = new System.Drawing.Point(188, 151);
-            this.maskedTextBox6.Name = "maskedTextBox6";
-            this.maskedTextBox6.Size = new System.Drawing.Size(122, 22);
-            this.maskedTextBox6.TabIndex = 6;
-            this.maskedTextBox6.Text = "Enter your Grade";
-            this.maskedTextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
@@ -108,13 +68,12 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Name";
             // 
-            // textBox1
+            // StudentName
             // 
-            this.textBox1.Location = new System.Drawing.Point(118, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 22);
-            this.textBox1.TabIndex = 8;
-            this.textBox1.Text = "Enter your Name";
+            this.StudentName.Location = new System.Drawing.Point(118, 45);
+            this.StudentName.Name = "StudentName";
+            this.StudentName.Size = new System.Drawing.Size(239, 22);
+            this.StudentName.TabIndex = 8;
             // 
             // label2
             // 
@@ -161,23 +120,130 @@
             this.label7.TabIndex = 14;
             this.label7.Text = "Algebra 101";
             // 
+            // finalGPA
+            // 
+            this.finalGPA.AutoSize = true;
+            this.finalGPA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalGPA.Location = new System.Drawing.Point(270, 365);
+            this.finalGPA.Name = "finalGPA";
+            this.finalGPA.Size = new System.Drawing.Size(57, 25);
+            this.finalGPA.TabIndex = 15;
+            this.finalGPA.Text = "GPA";
+            this.finalGPA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EnglishGrade
+            // 
+            this.EnglishGrade.Location = new System.Drawing.Point(203, 106);
+            this.EnglishGrade.Name = "EnglishGrade";
+            this.EnglishGrade.Size = new System.Drawing.Size(124, 22);
+            this.EnglishGrade.TabIndex = 16;
+            this.EnglishGrade.Tag = "Enter your Grade";
+            this.EnglishGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BiologyGrade
+            // 
+            this.BiologyGrade.Location = new System.Drawing.Point(203, 197);
+            this.BiologyGrade.Name = "BiologyGrade";
+            this.BiologyGrade.Size = new System.Drawing.Size(124, 22);
+            this.BiologyGrade.TabIndex = 17;
+            this.BiologyGrade.Tag = "Enter your Grade";
+            this.BiologyGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AlgebraGrade
+            // 
+            this.AlgebraGrade.Location = new System.Drawing.Point(203, 151);
+            this.AlgebraGrade.Name = "AlgebraGrade";
+            this.AlgebraGrade.Size = new System.Drawing.Size(124, 22);
+            this.AlgebraGrade.TabIndex = 18;
+            this.AlgebraGrade.Tag = "Enter your Grade";
+            this.AlgebraGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ComputerScienceGrade
+            // 
+            this.ComputerScienceGrade.Location = new System.Drawing.Point(203, 240);
+            this.ComputerScienceGrade.Name = "ComputerScienceGrade";
+            this.ComputerScienceGrade.Size = new System.Drawing.Size(124, 22);
+            this.ComputerScienceGrade.TabIndex = 19;
+            this.ComputerScienceGrade.Tag = "Enter your Grade";
+            this.ComputerScienceGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PsychologyGrade
+            // 
+            this.PsychologyGrade.Location = new System.Drawing.Point(203, 279);
+            this.PsychologyGrade.Name = "PsychologyGrade";
+            this.PsychologyGrade.Size = new System.Drawing.Size(124, 22);
+            this.PsychologyGrade.TabIndex = 20;
+            this.PsychologyGrade.Tag = "Enter your Grade";
+            this.PsychologyGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(413, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Grade Reference";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(413, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 16);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "A = 4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(413, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 16);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "B = 3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(412, 173);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 16);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "D = 1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(413, 157);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(36, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "C = 2";
+            // 
             // StudentGPACalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PsychologyGrade);
+            this.Controls.Add(this.ComputerScienceGrade);
+            this.Controls.Add(this.AlgebraGrade);
+            this.Controls.Add(this.BiologyGrade);
+            this.Controls.Add(this.EnglishGrade);
+            this.Controls.Add(this.finalGPA);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StudentName);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.maskedTextBox6);
-            this.Controls.Add(this.maskedTextBox5);
-            this.Controls.Add(this.maskedTextBox4);
-            this.Controls.Add(this.maskedTextBox3);
-            this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "StudentGPACalculator";
             this.Text = "StudentGPACalculator";
@@ -189,18 +255,24 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox6;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox StudentName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label finalGPA;
+        private System.Windows.Forms.TextBox EnglishGrade;
+        private System.Windows.Forms.TextBox BiologyGrade;
+        private System.Windows.Forms.TextBox AlgebraGrade;
+        private System.Windows.Forms.TextBox ComputerScienceGrade;
+        private System.Windows.Forms.TextBox PsychologyGrade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,30 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class StudentGPACalculator : Form
     {
+        private int course1Credit = 3;
+        private int course2Credit = 3;
+        private int course3Credit = 4;
+        private int course4Credit = 4;
+        private int course5Credit = 3;
+
+        private int englishGrade;
+        private int algebraGrade;
+        private int biologyGrade;
+        private int computerScienceGrade;
+        private int psychologyGrade;
+
+        private int totalGradePoints = 0;
+        private int totalCreditHours = 0;
         public StudentGPACalculator()
         {
             InitializeComponent();
+
+            EnglishGrade.KeyPress += GradeTextBox_KeyPress;
+            AlgebraGrade.KeyPress += GradeTextBox_KeyPress;
+            BiologyGrade.KeyPress += GradeTextBox_KeyPress;
+            ComputerScienceGrade.KeyPress += GradeTextBox_KeyPress;
+            PsychologyGrade.KeyPress += GradeTextBox_KeyPress;
         }
 
+        private void calculateTotalGrade()
+        {
+            int englishGrade = GetGradePoint(EnglishGrade.Text);
+            int algebraGrade = GetGradePoint(AlgebraGrade.Text);
+            int biologyGrade = GetGradePoint(BiologyGrade.Text);
+            int computerScienceGrade = GetGradePoint(ComputerScienceGrade.Text);
+            int psychologyGrade = GetGradePoint(PsychologyGrade.Text);
+
+            
+            totalGradePoints += course1Credit * englishGrade;
+            totalGradePoints += course2Credit * algebraGrade;
+            totalGradePoints += course3Credit * biologyGrade;
+            totalGradePoints += course4Credit * computerScienceGrade;
+            totalGradePoints += course5Credit * psychologyGrade;
+
+            totalCreditHours += course1Credit;
+            totalCreditHours += course2Credit;
+            totalCreditHours += course3Credit;
+            totalCreditHours += course4Credit;
+            totalCreditHours += course5Credit;
+
+        }
+        private int GetGradePoint(string grade)
+        {
+            switch (grade.ToUpper())
+            {
+                case "A":
+                    return 4;
+                case "B":
+                    return 3;
+                case "C":
+                    return 2;
+                case "D":
+                    return 1;
+                default:
+                    return 0; // Default to 0 for unknown grades
+            }
+        }
+
+        private void GradeTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            // Allow only A, B, C, and D
+            if (!(e.KeyChar == 'A' || e.KeyChar == 'B' || e.KeyChar == 'C' || e.KeyChar == 'D' ||
+                  e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete))
+            {
+                e.Handled = true; // Ignore the key press
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            calculateTotalGrade();
+            
+            decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+            int leadingDigit = (int)gradePointAverage;
+            int firstDigit = (int)(gradePointAverage * 10) % 10;
+            int secondDigit = (int)(gradePointAverage * 100) % 10;
 
-        }
+            string gpaText = string.Format("{0}.{1}{2}", leadingDigit, firstDigit, secondDigit);
 
-        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-
+            finalGPA.Text = gpaText;
         }
     }
 }
